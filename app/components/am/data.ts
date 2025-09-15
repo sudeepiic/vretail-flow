b60dadb944645d95bf28fcd9ec257d9d6a6df790
harshdodiya711 <harsh.dodiya.iic.0144@gmail.com>
2025-09-15 15:44:33 +0530

    update: am data added

--- app/components/am/data.ts
+++ app/components/am/data.ts
@@ -3,83 +3,201 @@ export interface CardData {
   image: string;
   title: string;
   description: string;
+  features: Array<string>;
 }
 
 export const cards: CardData[] = [
   {
     id: 1,
     image: "/flow-images/am/login-page.png",
-    title: "Login ",
-    description: "Secure login for Account-Manager with email and password",
+    title: "Login",
+    description: "Secure login for Account Manager with email and password.",
+    features: [
+      "Protected with JWT",
+      "Forgot password option",
+      "Role-based access",
+    ],
   },
   {
     id: 2,
     image: "/flow-images/am/project-selection.png",
     title: "Project Selection",
     description:
-      "This Project Selection page lets account-manager choose from available projects before entering the workspace. Each project card displays name seat availability and status with navigation controls to browse between projects",
+      "Account Managers can select any one of the projects assigned by Admin ",
+    features: [
+      "View all assigned projects",
+      "Check Totals Seats in Project",
+      "See project status (Active/Inactive)",
+    ],
   },
   {
     id: 3,
     image: "/flow-images/am/seat-selection.png",
     title: "Seat Selection",
     description:
-      'The process begins with the Seat Selection phase, where Account Managers see available Chat, Call, and Call & Chat seats. Seats are marked as Selected or Unavailable, and they confirm their choice using the Confirm Seat button."',
+      "Account Managers can select available Chat, Call, or Call & Chat seats. Seats are marked as Selected or Unavailable, and choices are confirmed using the Confirm Seat button.",
+    features: [],
   },
+
   {
     id: 4,
     image: "/flow-images/am/main-dashboard.png",
     title: "Dashboard",
     description:
-      "The main dashboard allows account managers to see all online visitors in real time. They can send  invites, view the visitor’s location. The dashboard also includes a Help Request tab to view assistance requests from visitors and provides the ability to block unwanted visitors.",
+      "Centralized Dashboard for Account Manager to view and manage visitor sessions.",
+    features: [
+      "View real-time online visitors with info like browser, platform, and location",
+      "Set AM mode to filter Call Only, Chat Only or Call & Chat Only sessions",
+      "Send chat or call invites to online visitors",
+      "Ban unwanted visitors from further sessions",
+      "Toggle between Active, Inactive, and All visitors",
+      "Filter Online Visitor by specific time",
+      "View Selected Visitor previous Session History(Call and Chat), Location on Map, Session tags assigned by AM (Created by Admin), Visitor Basic Info",
+      "AM can ban or unban visitors from further sessions for any sensible reasons",
+      "Get real time Notification about sessions conducted by other AM for the selected project",
+      "View Detail Session History in History Tab",
+      "View real time visitor's help request details",
+      "Accept visitor's help request for a requested type session",
+      "Link to copy Project Url ",
+    ],
   },
   {
     id: 5,
-    image: "/flow-images/am/call&chat.png",
-    title: "Live Chat and Dashboard Interaction",
+    image: "/flow-images/am/custom-invite.png",
+    title: "Custom Invite",
     description:
-      "This interactive interface allows seamless management of live chat & call of visitor sessions. Key features include real-time communication, session duration tracking. Administrators can manage user tags, session history, and notes for personalized interactions. The dashboard offers control options, allowing admins to take control of the session and show brochures and amenities. A notification panel keeps track of updates and user interactions.",
+      "Send custom invites to Visitors for a session on desired time and date over email",
+    features: ["Pin Protect Custom Invite Session for Selective Projects"],
   },
   {
     id: 6,
-    image: "/flow-images/am/custom-invite.png",
-    title: "Custom-Invite",
-    description:
-      "The Custom Invite Page, accessible from the main dashboard, allows account managers to send personalized  invitations by entering a visitor’s email address.",
+    image: "/flow-images/am/send-note.png",
+    title: "Dashboard",
+    description: "Send Quick Notes to Selected Visitor in real time  ",
+    features: [],
   },
+
   {
     id: 7,
-    image: "/flow-images/am/analytics.png",
-    title: "Analytics",
-    description:
-      "The Analytics Page provides account managers with detailed insights into visitor interactions. It lists visitor details, including total calls, total chats, and total invites including performance metrics like  positive ratings and  negative ratings. The filter option allows users to customize the time range for viewing analytics . Account managers can also export the details of all connected visitor sessions for reporting and further analysis.",
+    image: "/flow-images/am/vault.png",
+    title: "Dashboard",
+    description: "Upload Documents to Vault for Detail Analysis ",
+    features: [],
   },
+
   {
     id: 8,
-    image: "/flow-images/am/invites.png",
-    title: "Invitations",
-    description:
-      "The Page lists all invitations sent to visitors. Account managers can view invitation details and export . This interface includes a search that allows users to search for specific invites based on user name",
+    image: "/flow-images/am/sessionDashboard.png",
+    title: "Session Started Dashboard",
+    description: "AM can start a call or chat session with the visitor",
+    features: [
+      "Interactive window with normal and full screen view of the showcase product can be controlled by AM during live session ",
+      "Mute, unmute, Video On/off, Disconnect Session, Take or Give Control of product viewing can be done by AM during live session",
+      "All the current session visitor Information like name, email, phone number, location, session history, special notes, call duration can be seen in the Dashboard",
+      "Chat with the visitor in real time using the chat widget",
+      "Update Visitor Info ",
+    ],
   },
   {
     id: 9,
-    image: "/flow-images/am/faq.png",
-    title: "Faq ",
+    image: "/flow-images/am/offers.png",
+    title: "Custom Offers",
     description:
-      "The FAQ Page provides visitors with quick answers to common questions",
+      "AM can send custom offers to visitor in real time during live session",
+    features: [],
   },
   {
     id: 10,
-    image: "/flow-images/am/ticket.png",
-    title: "Tickets ",
+    image: "/flow-images/am/gridNote.png",
+    title: "Table Notes",
     description:
-      "The Ticket Page allows account-manager to view and manage support tickets. It includes features for tracking existing tickets by priority (High, Medium, Low) and their status, as well as creating new tickets by specifying the issue type, setting the priority, and adding detailed information about the problem.",
+      "AM can use table notes to send quick notes in table view to visitor in real time during live session",
+    features: [],
   },
   {
     id: 11,
-    image: "/flow-images/am/ticket.png",
-    title: "Ticket Management and Collaboration Interface ",
+    image: "/flow-images/am/visitor-feedback.png",
+    title: "Visitor Feedback",
+    description: "Options to save feedback of visitor at session end",
+    features: [],
+  },
+  {
+    id: 12,
+    image: "/flow-images/am/join-modals.png",
+    title: "Attention Modals",
     description:
-      "This interface facilitates efficient ticket resolution by allowing support agents and admins to manage and track support tickets. It displays key ticket details such as priority and status, and includes a space for real-time conversations between the support team and admin. This communication ensures smooth collaboration to discuss issues and resolve tickets promptly.",
+      "Displays important alerts such as Account Manager login from another device, poor network connection, or when a visitor joins another session.",
+    features: [],
+  },
+
+  {
+    id: 13,
+    image: "/flow-images/am/analytics.png",
+    title: "Analytics",
+    description:
+      "Provides detailed insights into visitor interactions, including total calls, chats, and invites, along with performance metrics like positive and negative ratings. Includes time range filters and export options.",
+    features: [
+      "Track total calls, chats, and invites",
+      "Measure positive and negative ratings",
+      "Filter analytics by time range",
+      "Export visitor session data",
+    ],
+  },
+  {
+    id: 14,
+    image: "/flow-images/am/invites.png",
+    title: "Invitations",
+    description:
+      "Lists all invitations sent to visitors. Account Managers can view details, export data, and search for specific invites by visitor name.",
+    features: [
+      "View list of all invites",
+      "Filter by Date Range",
+      "Search invites by visitor name",
+      "Export invitation data",
+      "Check status of sent invites",
+    ],
+  },
+  {
+    id: 15,
+    image: "/flow-images/am/faq.png",
+    title: "FAQ",
+    description:
+      "Provides visitors with quick answers to common questions in an easy-to-access format.",
+    features: [
+      "List of common visitor questions",
+      "Quick answers for easy access",
+      "Categorized FAQs",
+      "Search within FAQs",
+    ],
+  },
+
+  {
+    id: 16,
+    image: "/flow-images/am/ticket-page.png",
+    title: "Support Tickets Panel",
+    description:
+      "Track, filter, and create support tickets for issues, feedback, or feature requests",
+    features: [
+      "Create new tickets with attachments",
+      "Set priority and type for each ticket",
+      "Filter by ticket status, type, or priority",
+      "View all tickets or just your own",
+      "Paginated ticket listing with search by ticket id",
+    ],
+  },
+  {
+    id: 17,
+    image: "/flow-images/am/ticket-detail-page.png",
+    title: "Ticket Conversation View",
+    description:
+      "Engage in ongoing ticket conversations with image/file attachments, and track ticket status and updates.",
+    features: [
+      "Detailed ticket view with metadata",
+      "Message threads with timestamps",
+      "Image and file attachments in chat",
+      "Ticket type, description, and priority clearly shown",
+      "View recently created tickets on the side panel",
+      "Status updates visible on the ticket detail page",
+    ],
   },
 ];
--- app/components/am/index.tsx
+++ app/components/am/index.tsx
@@ -7,53 +7,70 @@ import DataVault from "../data-vaults";
 import CollabCards from "../collab";
 
 const AmCards: React.FC = () => {
-
-
-
   return (
-    <div className="w-full px-[10rem]  mx-auto p-6 space-y-12">
+    <div className="w-full px-[10rem] mx-auto p-4 space-y-16 my-12">
       {cards.map((card, idx) => {
         const imageOnLeft = idx % 2 === 0;
-        const descriptionPoints = card.description.split(". ").filter(Boolean);
-
         return (
           <div
             key={card.id}
-            className="flex flex-col md:flex-row items-center justify-between bg-white rounded-3xl shadow-sm overflow-hidden border border-gray-100"
+            className="relative flex flex-col md:flex-row items-center justify-between bg-white rounded-3xl shadow-md border border-indigo-100 overflow-hidden p-6 
+                       hover:shadow-xl hover:-translate-y-1 hover:border-indigo-300 transition-all duration-300"
           >
+            {/* Floating badge */}
+            <div
+              className={`absolute bottom-4 ${
+                !imageOnLeft ? "left-4" : "right-4"
+              } text-indigo-200/20 text-[14rem] font-extrabold leading-none pointer-events-none select-none z-1`}
+            >
+              {card.id}
+            </div>
+            {/* Image left */}
             {imageOnLeft && (
-              <div className="w-full md:w-1/2 flex items-center justify-center p-4">
-                <img
-                  src={card.image}
-                  alt={card.title}
-                  className="w-full h-[300px] rounded-lg shadow-sm object-contain"
-                />
+              <div className="w-full md:w-3/4 flex items-center justify-center ">
+                <div className="w-full  h-[410px] flex items-center justify-center bg-indigo-50 rounded-2xl overflow-hidden">
+                  <img
+                    src={card.image}
+                    alt={card.title}
+                    className="w-full h-full object-contain object-center"
+                  />
+                </div>
               </div>
             )}
 
-            <div className="w-full md:w-1/2 p-8 text-center md:text-left">
-              <h3
-                className="text-3xl font-bold mb-6 text-violet-700 
-             bg-clip-text "
-              >{card.title}
+            {/* Content */}
+            <div className="w-full md:w-1/2 p-8  text-center md:text-left z-5">
+              <h3 className="text-2xl font-bold text-indigo-600 mb-4">
+                {card.title}
               </h3>
-              <ul className="text-gray-900 text-lg leading-relaxed max-w-lg mx-auto md:mx-0 space-y-3">
-                {descriptionPoints.map((point, i) => (
-                  <li key={i} className="flex items-start">
-                    <span className="text-violet-700 mr-3 mt-1">⦿</span>
-                    <span>{point.trim()}</span>
+
+              <p className="text-gray-600 leading-relaxed mb-6">
+                {card.description}
+              </p>
+
+              <ul className="space-y-3 text-base">
+                {card.features.map((feature, i) => (
+                  <li
+                    key={i}
+                    className="flex items-center gap-3 text-gray-700 transition-all duration-300 hover:translate-x-1 hover:text-indigo-600"
+                  >
+                    <CheckCircle className="w-5 h-5 text-indigo-500 flex-shrink-0" />
+                    <span>{feature}</span>
                   </li>
                 ))}
               </ul>
             </div>
 
+            {/* Image right */}
             {!imageOnLeft && (
-              <div className="w-full md:w-1/2 flex items-center justify-center p-6">
-                <img
-                  src={card.image}
-                  alt={card.title}
-                  className="w-full h-[300px] rounded-xl shadow-sm object-contain"
-                />
+              <div className="w-full md:w-3/4 flex items-center justify-center ">
+                <div className="w-full  h-[410px] flex items-center justify-center bg-indigo-50 rounded-2xl overflow-hidden">
+                  <img
+                    src={card.image}
+                    alt={card.title}
+                    className="w-full h-full object-contain "
+                  />
+                </div>
               </div>
             )}
           </div>
